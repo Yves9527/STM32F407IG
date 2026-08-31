457bdfb8848459cab7a93d069f8f72165ad81173
Yves.He <91321570+Yves9527@users.noreply.github.com>
2026-08-31 19:52:04 +0800

    增加：实验6-9 直流有刷电机-电流环+位置环双环控制实验

---
+++ Motor_Control_Examples/Exp6_9_BrushedDCMotor_CurrentPositionDualLoop/Drivers/BSP/PID/pid.h
@@ -0,0 +1,86 @@
+/**
+ ****************************************************************************************************
+ * @file        pid.h
+ * @author      正点原子团队(ALIENTEK)
+ * @version     V1.0
+ * @date        2021-10-14
+ * @brief       PID代码
+ * @license     Copyright (c) 2020-2032, 广州市星翼电子科技有限公司
+ ****************************************************************************************************
+ * @attention
+ *
+ * 实验平台:正点原子 F407电机开发板
+ * 在线视频:www.yuanzige.com
+ * 技术论坛:www.openedv.com/forum.php
+ * 公司网址:www.alientek.com
+ * 购买地址:openedv.taobao.com
+ *
+ * 修改说明
+ * V1.0 20211014
+ * 第一次发布
+ *
+ ****************************************************************************************************
+ */
+#ifndef __PID_H
+#define __PID_H
+
+#include "./SYSTEM/sys/sys.h"
+
+/******************************************************************************************/
+/* PID相关参数 */
+
+#define  INCR_LOCT_SELECT  0         /* 0：位置式，1：增量式 */
+
+/* 注意：双环控制的时候，外环PID参数调节幅度不要太大，这对于整个曲线的影响很大 */
+
+#if INCR_LOCT_SELECT
+
+/* 定义位置环PID参数相关宏 */
+#define  L_KP      0.18f             /* P参数 */
+#define  L_KI      0.00f             /* I参数 */
+#define  L_KD      0.08f             /* D参数 */
+
+/* 定义电流环PID参数相关宏 */
+#define  C_KP      10.00f            /* P参数 */
+#define  C_KI      9.00f             /* I参数 */
+#define  C_KD      0.00f             /* D参数 */
+#define SMAPLSE_PID_SPEED  50        /* 采样周期 单位ms */
+
+#else
+
+/* 定义位置环PID参数相关宏 */
+#define  L_KP      0.18f             /* P参数 */
+#define  L_KI      0.00f             /* I参数 */
+#define  L_KD      0.08f             /* D参数 */
+
+/* 定义电流环PID参数相关宏 */
+#define  C_KP      10.00f            /* P参数 */
+#define  C_KI      7.00f             /* I参数 */
+#define  C_KD      0.00f             /* D参数 */
+#define SMAPLSE_PID_SPEED  50        /* 采样周期 单位ms */
+
+#endif
+
+/*PID结构体*/
+typedef struct
+{
+    __IO float  SetPoint;            /* 目标值 */
+    __IO float  ActualValue;         /* 期望输出值 */
+    __IO float  SumError;            /* 误差累计 */
+    __IO float  Proportion;          /* 比例常数 P */
+    __IO float  Integral;            /* 积分常数 I */
+    __IO float  Derivative;          /* 微分常数 D */
+    __IO float  Error;               /* Error[1] */
+    __IO float  LastError;           /* Error[-1] */
+    __IO float  PrevError;           /* Error[-2] */
+} PID_TypeDef;
+
+extern PID_TypeDef  g_location_pid;  /* 位置环PID参数结构体 */
+extern PID_TypeDef  g_current_pid;   /* 电流环PID参数结构体 */
+
+/******************************************************************************************/
+
+void pid_init(void);                 /* pid初始化 */
+int32_t increment_pid_ctrl(PID_TypeDef *PID,float Feedback_value);      /* pid闭环控制 */
+
+#endif
---
+++ Motor_Control_Examples/Exp6_9_BrushedDCMotor_CurrentPositionDualLoop/User/main.c
@@ -0,0 +1,178 @@
+/**
+ ****************************************************************************************************
+ * @file        main.c
+ * @author      正点原子团队(ALIENTEK)
+ * @version     V1.0
+ * @date        2021-10-16
+ * @brief       直流有刷电机电流+位置双环PID 实验
+ * @license     Copyright (c) 2020-2032, 广州市星翼电子科技有限公司
+ ****************************************************************************************************
+ * @attention
+ *
+ * 实验平台:正点原子 F407电机开发板
+ * 在线视频:www.yuanzige.com
+ * 技术论坛:www.openedv.com/forum.php
+ * 公司网址:www.alientek.com
+ * 购买地址:openedv.taobao.com
+ *
+ ****************************************************************************************************
+ */
+
+#include "./SYSTEM/sys/sys.h"
+#include "./SYSTEM/usart/usart.h"
+#include "./SYSTEM/delay/delay.h"
+#include "./BSP/LED/led.h"
+#include "./BSP/KEY/key.h"
+#include "./BSP/LCD/lcd.h"
+#include "./BSP/DC_MOTOR/dc_motor.h"
+#include "./BSP/ADC/adc.h"
+#include "./BSP/TIMER/dcmotor_tim.h"
+#include "./BSP/PID/pid.h"
+#include "./DEBUG/debug.h"
+
+extern uint8_t  g_run_flag;
+
+void lcd_dis(void);
+
+int main(void)
+{
+    uint8_t key;
+    uint16_t t;
+    uint8_t debug_cmd = 0;
+
+    HAL_Init();                             /* 初始化HAL库 */
+    sys_stm32_clock_init(336, 8, 2, 7);     /* 设置时钟,168Mhz */
+    delay_init(168);                        /* 延时初始化 */
+    usart_init(115200);                     /* 串口1初始化，用于上位机调试 */
+    led_init();                             /* 初始化LED */
+    lcd_init();                             /* 初始化LCD */
+    key_init();                             /* 初始化按键 */
+    pid_init();                             /* 初始化PID参数 */
+    atim_timx_cplm_pwm_init(8400 - 1 , 0);  /* 168 000 000 / 1 = 168 000 000 168Mhz的计数频率，计数8400次为50us */
+    dcmotor_init();                         /* 初始化电机 */
+    gtim_timx_encoder_chy_init(0XFFFF, 0);  /* 编码器定时器初始化，不分频直接84M的计数频率 */
+    btim_timx_int_init(1000 - 1 , 84 - 1);  /* 基本定时器初始化，1ms计数周期 */
+    adc_nch_dma_init();                     /* 初始化ADC、DMA */
+    
+#if DEBUG_ENABLE                            /* 开启调试 */
+    
+    debug_init();                           /* 初始化调试 */
+    debug_send_motorcode(DC_MOTOR);         /* 上传电机类型（直流有刷电机） */
+    debug_send_motorstate(IDLE_STATE);      /* 上传电机状态（空闲） */
+    
+    /* 同步数据PID参数到上位机 ，无论同步哪一组数据，目标值地址只能是外环PID的 */
+    debug_send_initdata(TYPE_PID1, (float *)(&g_location_pid.SetPoint), L_KP, L_KI, L_KD);  /* 位置环PID参数（PID1）*/
+    debug_send_initdata(TYPE_PID2, (float *)(&g_location_pid.SetPoint), C_KP, C_KI, C_KD);  /* 电流环PID参数（PID2）*/
+
+#endif
+
+    g_point_color = WHITE;
+    g_back_color  = BLACK;
+    lcd_show_string(10, 10, 200, 16, 16, "DcMotor Test", g_point_color);
+    lcd_show_string(10, 30, 200, 16, 16, "KEY0:Start forward", g_point_color);
+    lcd_show_string(10, 50, 200, 16, 16, "KEY1:Start backward", g_point_color);
+    lcd_show_string(10, 70, 200, 16, 16, "KEY2:Stop", g_point_color);
+
+    while (1)
+    {
+        key = key_scan(0);                                  /* 按键扫描 */
+        if(key == KEY0_PRES)                                /* 当key0按下 */
+        {
+            g_run_flag = 1;                                 /* 标记电机启动 */
+            dcmotor_start();                                /* 开启电机 */
+            g_location_pid.SetPoint += 1320;                /* 正转一圈，电机旋转圈数 = 编码器总计数值 / 44 / 30 */
+            
+            if (g_location_pid.SetPoint >= 6600)            /* 限制电机位置（正转最大5圈） */
+            {
+                g_location_pid.SetPoint = 6600;
+            }
+#if DEBUG_ENABLE
+            debug_send_motorstate(RUN_STATE);               /* 上传电机状态（运行） */
+#endif
+        }
+        
+        else if(key == KEY1_PRES)                           /* 当key1按下 */
+        {
+            g_run_flag = 1;                                 /* 标记电机启动 */
+            dcmotor_start();                                /* 开启电机 */
+            g_location_pid.SetPoint -= 1320;                /* 反转一圈 */
+            
+            if (g_location_pid.SetPoint <= -6600)           /* 限制电机位置（反转最大5圈） */
+            {
+                g_location_pid.SetPoint = -6600;
+            }
+#if DEBUG_ENABLE
+            debug_send_motorstate(RUN_STATE);               /* 上传电机状态（运行） */
+#endif
+        }
+        
+        else if(key == KEY2_PRES)                           /* 当key2按下 */
+        {
+            g_location_pid.SetPoint = 0;                    /* 恢复初始位置 */
+        }
+
+#if DEBUG_ENABLE
+
+        /* 接收PID助手设置的位置环PID参数 */
+        debug_receive_pid(TYPE_PID1, (float *)&g_location_pid.Proportion,(float *)&g_location_pid.Integral, (float *)&g_location_pid.Derivative);
+
+        /* 接收PID助手设置的速度环PID参数 */
+        debug_receive_pid(TYPE_PID2, (float *)&g_current_pid.Proportion, (float *)&g_current_pid.Integral, (float *)&g_current_pid.Derivative);
+        
+        debug_set_point_range(6600, -6600, 6600);                       /* 设置目标调节范围 */
+        
+        debug_cmd = debug_receive_ctrl_code();                          /* 读取上位机指令 */
+
+        if (debug_cmd == HALT_CODE)                                     /* 电机停机 */
+        {
+            g_location_pid.SetPoint = 0;                                /* 恢复初始位置 */
+        } 
+        else if (debug_cmd == RUN_CODE)                                 /* 电机运行 */
+        { 
+            g_run_flag = 1;                                             /* 标记电机启动 */
+            dcmotor_start();                                            /* 开启电机 */
+            g_location_pid.SetPoint = 1320;                             /* 设置目标位置 */
+            debug_send_motorstate(RUN_STATE);                           /* 上传电机状态（运行） */
+        }
+#endif
+        t++;
+        if(t % 20 == 0)
+        {
+            lcd_dis();
+            LED0_TOGGLE();                                              /* LED0(红灯) 翻转 */
+#if DEBUG_ENABLE
+            debug_send_current(g_motor_data.current,0,0);               /* 发送电流到U相 */
+            g_debug.encode_p = g_motor_data.location;                   /* 传入编码器当前总计数值 */
+            debug_upload_data(&g_debug, TYPE_HAL_ENC);                  /* 发送编码器当前总计数值 */
+#endif
+        }
+        delay_ms(10);
+    }
+}
+
+/**
+ * @brief       数据显示函数
+ * @param       无
+ * @retval      无
+ */
+void lcd_dis(void)
+{
+    char buf[32];
+    
+    /* 显示占空比 */
+    sprintf(buf, "PWM_Duty:%.1f%c  ", (float)(g_motor_data.motor_pwm * 100 / 8400), '%');
+    lcd_show_string(10, 150, 200, 16, 16, buf, g_point_color);
+
+    /* 显示目标位置 */
+    sprintf(buf, "LocationSet:%3d ", (int)g_location_pid.SetPoint);
+    lcd_show_string(10, 170, 200, 16, 16, buf, g_point_color);
+
+    /* 显示实际位置 */
+    sprintf(buf, "Location:%3d ", (int)g_motor_data.location);
+    lcd_show_string(10, 190, 200, 16, 16, buf, g_point_color);
+    
+    /* 显示实际电流 */
+    sprintf(buf, "Current:%.3fmA ", g_motor_data.current);
+    lcd_show_string(10, 210, 200, 16, 16, buf, g_point_color);
+}
+
